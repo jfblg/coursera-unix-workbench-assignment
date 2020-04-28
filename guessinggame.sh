902d8333c2f3692900e50bfc782954dee59669e1
jfblg <inbox.jfblg@gmail.com>
2020-04-28 21:03:17 +0200

    init

---
+++ guessinggame.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Guess how many files are in the current directory:"
+read user_guess
+
+# TODO: find out how many files are in dir, store it and compare it with users input
+# TODO: too low, too high prompt and prompt again
+# TODO must contain 1 function, 1 loop, 1 if statement
+# TODO more than 20, less than 50 lines of code
+
---
+++ makefile
@@ -0,0 +1,6 @@
+makefile 
+
+this make file should produce README.md
+title of the project
+data and time at which the make was run
+number of lines of code in guessinggame.sh
